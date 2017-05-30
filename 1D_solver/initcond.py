ebdca232aee5082335eb8ee4d95aef274a7fc947
nikichats <niki.c27@hotmail.com>
2017-05-30 08:58:07 +0100

    initial conditions from Toro

---
+++ 1D_solver/initcond.py
@@ -0,0 +1,21 @@
+# Initial conditions for the 1D_Euler.py program. Cases (1)-(5) are from Toro pg. 129
+
+def sod():
+    U_0 = [1.0, 0.125, 0.0, 0.0, 1.0, 0.1]
+    return U_0
+
+def toro_2():
+    U_0 = [1.0, 1.0, -2.0, 2.0, 0.4, 0.4]
+    return U_0
+
+def toro_3():
+    U_0 = [1.0, 1.0, 0.0, 0.0, 1000., 0.1]
+    return U_0
+
+def toro_4():
+    U_0 = [1.0, 1.0, 0.0, 0.0, 0.01, 100.]
+    return U_0
+
+def toro_5():
+    U_0 = [5.99924, 5.99924, 12.5975, -6.19633, 460.894, 46.095]
+    return U_0
