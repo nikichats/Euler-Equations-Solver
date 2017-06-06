548455eccc55a9c4498f26ba851745a9cb8579d8
nikichats <niki.c27@hotmail.com>
2017-06-06 10:26:47 +0100

    RichtM looking beautiful

--- Euler_1D/flux.py
+++ Euler_1D/flux.py
@@ -105,11 +105,11 @@ def MacCormack(density, momentum, energy, ghost_nx, nx, gamma, dt, dx):
     energy_predictor = np.zeros(nx)
 
     # (1) prediction step
-    [flux1_density, flux1_momentum, flux1_energy] = flux_cc(density, momentum, energy, gamma)
+    [flux1_density, flux1_momentum, flux1_energy] = flux_fc(density, momentum, energy, gamma)
     for i in range(ghost_nx, nx - ghost_nx):
-        density_predictor[i] = density[i]  - (dt/dx) * (flux1_density[i+1] - flux1_density[i])
-        momentum_predictor[i] = momentum[i] - (dt/dx) * (flux1_momentum[i+1] - flux1_momentum[i])
-        energy_predictor[i] = energy[i] - (dt/dx) * (flux1_energy[i+1] - flux1_energy[i])
+        density_predictor[i] = density[i]  - (dt/dx) * (flux1_density[i] - flux1_density[i-1])
+        momentum_predictor[i] = momentum[i] - (dt/dx) * (flux1_momentum[i] - flux1_momentum[i-1])
+        energy_predictor[i] = energy[i] - (dt/dx) * (flux1_energy[i] - flux1_energy[i-1])
 
     density_predictor = f.boundary_conditions(density_predictor, ghost_nx, nx)
     momentum_predictor = f.boundary_conditions(momentum_predictor, ghost_nx, nx)
@@ -125,7 +125,7 @@ def MacCormack(density, momentum, energy, ghost_nx, nx, gamma, dt, dx):
     return density_new, momentum_new, energy_new
 
 
-def RichtMyer(density, momentum, energy, ghost_nx, nx, gamma, dt, dx):
+def RichtMyer_Smart(density, momentum, energy, ghost_nx, nx, gamma, dt, dx, CFL):
     density_new = np.zeros(nx)
     momentum_new = np.zeros(nx)
     energy_new = np.zeros(nx)
@@ -136,9 +136,9 @@ def RichtMyer(density, momentum, energy, ghost_nx, nx, gamma, dt, dx):
     # (1) prediction step
     [flux1_density, flux1_momentum, flux1_energy] = flux_cc(density, momentum, energy, gamma)
     for i in range(ghost_nx, nx - ghost_nx):
-        density_predictor[i] = 0.5 * (density[i + 1] + density[i - 1]) - 0.5 * (0.5 * dt/dx) * (flux1_density[i+1] - flux1_density[i-1])
-        momentum_predictor[i] = 0.5 * (momentum[i + 1] + momentum[i - 1]) - 0.5 * (0.5 * dt/dx) * (flux1_momentum[i+1] - flux1_momentum[i-1])
-        energy_predictor[i] = 0.5 * (energy[i + 1] + energy[i - 1]) - 0.5 * (0.5 * dt/dx) * (flux1_energy[i+1] - flux1_energy[i-1])
+        density_predictor[i] = 0.5 * (density[i + 1] + density[i]) -   (0.5 * dt/dx) * (flux1_density[i+1] - flux1_density[i])
+        momentum_predictor[i] = 0.5 * (momentum[i + 1] + momentum[i]) -   (0.5 * dt/dx) * (flux1_momentum[i+1] - flux1_momentum[i])
+        energy_predictor[i] = 0.5 * (energy[i + 1] + energy[i]) -   (0.5 * dt/dx) * (flux1_energy[i+1] - flux1_energy[i])
 
     density_predictor = f.boundary_conditions(density_predictor, ghost_nx, nx)
     momentum_predictor = f.boundary_conditions(momentum_predictor, ghost_nx, nx)
@@ -148,14 +148,8 @@ def RichtMyer(density, momentum, energy, ghost_nx, nx, gamma, dt, dx):
 
     [flux2_density, flux2_momentum, flux2_energy] = flux_cc(density_predictor, momentum_predictor, energy_predictor, gamma)
     for i in range(ghost_nx, nx - ghost_nx):
-        density_new[i] = density[i] - 0.5 * (dt/dx) * (flux2_density[i+1] - flux2_density[i-1])
-        momentum_new[i] = momentum[i] - 0.5 * (dt/dx) * (flux2_momentum[i+1] - flux2_momentum[i-1])
-        energy_new[i] = energy[i] - 0.5 * (dt/dx) * (flux2_energy[i+1] - flux2_energy[i-1])
-
-    # [flux2_density, flux2_momentum, flux2_energy] = flux_fc(density_predictor, momentum_predictor, energy_predictor, gamma)
-    # for i in range(ghost_nx, nx - ghost_nx):
-    #     density_new[i] = density[i] - 0.5 * (dt/dx) * (flux2_density[i] - flux2_density[i-1])
-    #     momentum_new[i] = momentum[i] - 0.5 * (dt/dx) * (flux2_momentum[i] - flux2_momentum[i-1])
-    #     energy_new[i] = energy[i] - 0.5 * (dt/dx) * (flux2_energy[i] - flux2_energy[i-1])
+        density_new[i] = density[i] -  (dt/dx) * (flux2_density[i] - flux2_density[i-1])
+        momentum_new[i] = momentum[i] -  (dt/dx) * (flux2_momentum[i] - flux2_momentum[i-1])
+        energy_new[i] = energy[i] -  (dt/dx) * (flux2_energy[i] - flux2_energy[i-1])
 
     return density_new, momentum_new, energy_new
--- Euler_1D/main.py
+++ Euler_1D/main.py
@@ -4,7 +4,7 @@ import initial_conds as ic
 import functions as f
 import flux
 
-domain_nx = 50
+domain_nx = 1000
 ghost_nx = 2
 nx = domain_nx + 2*ghost_nx
 x_min = -10.
@@ -14,7 +14,7 @@ dx = (x_max - x_min) / domain_nx
 
 x = np.linspace(x_min - ghost_nx * dx, x_max + ghost_nx * dx, nx + 1)
 time_max = 0.01
-CFL = 0.3
+CFL = 0.4
 gamma = 1.4
 eps_AV = 0.0
 
@@ -27,7 +27,7 @@ u_L = 0.0
 u_R = 0.0
 p_L = 1.e5
 p_R = 1.e4
-shock_position = 0.5
+shock_position = 0.0
 
 [density, momentum, energy, velocity, pressure] = ic.initial_cond(x, rho_L, rho_R, u_L, u_R, p_L, p_R, gamma, nx, dx, shock_position)
 
@@ -37,13 +37,25 @@ time_step = 0
 f.write(x, density, momentum, energy, time_step, solver, gamma)
 
 while time < time_max:
+
+    # apply boundary conditions
+    density = f.boundary_conditions(density, ghost_nx, nx)
+    momentum = f.boundary_conditions(momentum, ghost_nx, nx)
+    energy = f.boundary_conditions(energy, ghost_nx, nx)
+
     # time stepping
     dt = f.time_step(density, velocity, pressure, ghost_nx, nx, dx, CFL, gamma)
-    dt = 4.276e-4
+    #dt = 4.276e-4
     if dt < 1e-7:
         break
     time = time + dt
     time_step = time_step + 1
+
+    # apply artificial viscosity
+    epsilon = 0.05
+    density = f.artificial_viscosity(density, ghost_nx, nx, epsilon)
+    momentum = f.artificial_viscosity(momentum, ghost_nx, nx, epsilon)
+    energy  = f.artificial_viscosity(energy, ghost_nx, nx, epsilon)
 
     # calculate new quantities
     if solver == 2:
@@ -51,9 +63,16 @@ while time < time_max:
     elif solver == 3:
         [density_new, momentum_new, energy_new] = flux.MacCormack(density, momentum, energy, ghost_nx, nx, gamma, dt, dx)
     elif solver == 4:
-        [density_new, momentum_new, energy_new] = flux.RichtMyer(density, momentum, energy, ghost_nx, nx, gamma, dt, dx)
+        [density_new, momentum_new, energy_new] = flux.RichtMyer_Smart(density, momentum, energy, ghost_nx, nx, gamma, dt, dx, CFL)
     else:
         [density_new, momentum_new, energy_new] = flux.basic(density, momentum, energy, ghost_nx, nx, gamma, dt, dx)
+
+
+
+    # # apply boundary conditions
+    # density_new = f.boundary_conditions(density_new, ghost_nx, nx)
+    # momentum_new = f.boundary_conditions(momentum_new, ghost_nx, nx)
+    # energy_new = f.boundary_conditions(energy_new, ghost_nx, nx)
 
     # apply boundary conditions
     density_new = f.boundary_conditions(density_new, ghost_nx, nx)
@@ -67,6 +86,7 @@ while time < time_max:
     velocity = momentum / density
     pressure = (gamma - 1) * (energy - 0.5 * (momentum**2) / density)
 
+    print(time, dt)
     f.write(x, density, momentum, energy, time_step, solver, gamma)
 
 print("final time step = " + str(time_step))
